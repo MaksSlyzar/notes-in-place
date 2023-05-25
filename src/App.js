d1fbb32976fe30844ef60469714f860057bc5724
MaksSlyzar <maks.slyzar@gmail.com>
2023-05-25 21:00:00 +0300

    add tests

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import './App.scss';
-import Directories from './components/Directories/Directories';
-import NavigationPanel from './components/NavigationPanel/NavigationPanel';
-import NodeEditor from './components/NoteEditor/NoteEditor';
-
+import "./App.scss";
+import Directories from "./components/Directories/Directories";
+import NavigationPanel from "./components/NavigationPanel/NavigationPanel";
+import NodeEditor from "./components/NoteEditor/NoteEditor";
 
 function App() {
   return (
     <div className="App">
       {/* <NavigationPanel/> */}
-      <NodeEditor/>
-      <Directories />
+      <NodeEditor />
     </div>
   );
 }
